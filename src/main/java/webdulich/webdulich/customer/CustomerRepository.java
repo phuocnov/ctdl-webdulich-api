6e2da4713c6cc75898ef6ea0655351bede990e38
phuocnov <phuocnguyenhuu806@gmail.com>
2022-03-03 08:32:45 +0700

    fix-1

--- src/main/java/webdulich/webdulich/customer/CustomerRepository.java
+++ src/main/java/webdulich/webdulich/customer/CustomerRepository.java
@@ -1,11 +1,8 @@
 package webdulich.webdulich.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.lang.StackWalker.Option;
 import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
