449f0660f7d5ee7e7c2b4d0f471aeef6a645fc53
phuocnov <phuocnguyenhuu806@gmail.com>
2022-03-16 08:28:52 +0700

    fix cors

--- src/main/java/webdulich/webdulich/WebdulichApplication.java
+++ src/main/java/webdulich/webdulich/WebdulichApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import webdulich.webdulich.role.RoleRepository;
 import webdulich.webdulich.role.Role;
--- src/main/java/webdulich/webdulich/customer/CustomerController.java
+++ src/main/java/webdulich/webdulich/customer/CustomerController.java
@@ -8,6 +8,7 @@ import javax.management.relation.Role;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
