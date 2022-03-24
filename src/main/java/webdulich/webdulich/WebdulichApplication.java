145caafcbfd60dc37c56238ef6f594400596b274
phuocnov <phuocnguyenhuu806@gmail.com>
2022-03-24 21:12:05 +0700

    close role init

--- src/main/java/webdulich/webdulich/WebdulichApplication.java
+++ src/main/java/webdulich/webdulich/WebdulichApplication.java
@@ -31,17 +31,4 @@ public class WebdulichApplication implements CommandLineRunner {
 			}
 		};
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Role adminRole = new Role();
-		adminRole.setName("ROLE_ADMIN");
-		roleRepository.save(adminRole);
-
-		Role userRole = new Role();
-		userRole.setName("ROLE_USER");
-		roleRepository.save(userRole);
-
-	}
 }
